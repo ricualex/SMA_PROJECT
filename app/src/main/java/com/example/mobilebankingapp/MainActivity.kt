84d0c828cf9066b4bc01be85c6bbce12aad963f0
Ricu Alexandru <alexandruricu25@gmail.com>
2023-12-29 14:53:26 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mobilebankingapp/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.mobilebankingapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.mobilebankingapp.screens.HomeScreen
+import com.example.mobilebankingapp.screens.LoginScreen
+import com.example.mobilebankingapp.ui.theme.MobileBankingAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MobileBankingAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    App()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController = navController , startDestination = "login") {
+        composable("login") {
+            LoginScreen(navController = navController)
+        }
+        composable("homepage") {
+            HomeScreen(navController = navController)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/mobilebankingapp/screens/LoginScreen.kt
@@ -0,0 +1,32 @@
+package com.example.mobilebankingapp.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+    ) {
+        Text(text = "Login")
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController.navigate("homepage") }) {
+            Icon(Icons.Default.ArrowForward, contentDescription = null)
+            Text(text = "Login")
+        }
+    }
+}
