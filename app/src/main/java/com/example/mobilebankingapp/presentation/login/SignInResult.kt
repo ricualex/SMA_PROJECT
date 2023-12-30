eb68949cba4dc7541c4ea97fc660ee5cc51434cb
Ricu Alexandru <alexandruricu25@gmail.com>
2023-12-30 02:45:11 +0200

    Firebase setup for app

---
+++ app/src/main/java/com/example/mobilebankingapp/presentation/login/SignInResult.kt
@@ -0,0 +1,3 @@
+package com.example.mobilebankingapp.presentation.login
+
+data class SignInResult()
--- app/src/main/java/com/example/mobilebankingapp/screens/SignInScreen.kt
+++ app/src/main/java/com/example/mobilebankingapp/screens/SignInScreen.kt
@@ -1,5 +1,7 @@
 package com.example.mobilebankingapp.screens
 
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,9 +13,13 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.mobilebankingapp.presentation.login.SignInState
 
 @Composable
 fun LoginScreen(navController: NavController) {
@@ -29,4 +35,33 @@ fun LoginScreen(navController: NavController) {
             Text(text = "Login")
         }
     }
+}
+
+@Composable
+fun SignInScreen(
+    state: SignInState,
+    onSignInClick: () -> Unit
+) {
+    val context = LocalContext.current
+    LaunchedEffect(key1 = state.signInError) {
+        state.signInError?.let { error ->
+            Toast.makeText(
+                context,
+                error,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = onSignInClick) {
+            Text(text = "SignIn")
+        }
+        
+    }
+
 }
