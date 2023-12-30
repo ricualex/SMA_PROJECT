44322a3ce791d3bfede1fe9b2badb8bee09e1439
Ricu Alexandru <alexandruricu25@gmail.com>
2023-12-30 02:48:08 +0200

    Authentication using Gmail

--- app/src/main/java/com/example/mobilebankingapp/presentation/login/SignInResult.kt
+++ app/src/main/java/com/example/mobilebankingapp/presentation/login/SignInResult.kt
@@ -1,3 +1,12 @@
 package com.example.mobilebankingapp.presentation.login
 
-data class SignInResult()
+data class SignInResult(
+    val data: UserData?,
+    val errorMessage: String?
+)
+
+data class UserData(
+    val userId: String,
+    val username: String?,
+    val profilePictureUrl: String?
+)
--- app/src/main/java/com/example/mobilebankingapp/screens/SignInScreen.kt
+++ app/src/main/java/com/example/mobilebankingapp/screens/SignInScreen.kt
@@ -1,41 +1,40 @@
 package com.example.mobilebankingapp.screens
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
+import androidx.compose.ui.unit.sp
+import com.example.mobilebankingapp.R
+import com.example.mobilebankingapp.components.BoldTextComponent
+import com.example.mobilebankingapp.components.DefaultBlueButton
+import com.example.mobilebankingapp.components.NormalTextComponent
 import com.example.mobilebankingapp.presentation.login.SignInState
-
-@Composable
-fun LoginScreen(navController: NavController) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-    ) {
-        Text(text = "Login")
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { navController.navigate("homepage") }) {
-            Icon(Icons.Default.ArrowForward, contentDescription = null)
-            Text(text = "Login")
-        }
-    }
-}
 
 @Composable
 fun SignInScreen(
@@ -52,16 +51,30 @@ fun SignInScreen(
             ).show()
         }
     }
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Button(onClick = onSignInClick) {
-            Text(text = "SignIn")
+    Column {
+        val logoPainter = painterResource(id = R.drawable.bank_logo)
+        BoldTextComponent("Mobile Banking App")
+        NormalTextComponent("Hello there,")
+        NormalTextComponent("Welcome back")
+        Spacer(modifier = Modifier
+            .heightIn(40.dp)
+        )
+        Box(
+            modifier = Modifier
+                .align(CenterHorizontally)
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            DefaultBlueButton(value = "Sign In With Google", onSignInClick = onSignInClick)
         }
-        
+        Image(
+            painter = logoPainter,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .clip(MaterialTheme.shapes.medium)
+                .padding(16.dp)
+                .align(Alignment.CenterHorizontally)
+        )
     }
-
 }
