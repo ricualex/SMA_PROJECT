16200d519c228ead2e88d6a82e73afee634af9bf
Ricu Alexandru <alexandruricu25@gmail.com>
2024-01-01 17:05:03 +0200

    Data encryption, not functional encrypt function, WiP

--- app/src/main/java/com/example/mobilebankingapp/firebase/DataEncryption.kt
+++ app/src/main/java/com/example/mobilebankingapp/firebase/DataEncryption.kt
@@ -1,2 +1,46 @@
 package com.example.mobilebankingapp.firebase
 
+import android.content.Context
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import java.security.Key
+import java.security.KeyStore
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import kotlin.coroutines.CoroutineContext
+
+fun encryptData(plainText: String, secretKey: Key): ByteArray {
+    val cipher = Cipher.getInstance("AES")
+    cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+    return cipher.doFinal(plainText.toByteArray())
+}
+
+fun decryptData(cipherText: ByteArray, secretKey: Key): String {
+    val cipher = Cipher.getInstance("AES")
+    cipher.init(Cipher.DECRYPT_MODE, secretKey)
+    val decryptedBytes = cipher.doFinal(cipherText)
+    return String(decryptedBytes)
+}
+
+fun generateSecretKey(context: Context, keyAlias: String): SecretKey {
+    val keyStore = KeyStore.getInstance("AndroidKeyStore")
+    keyStore.load(null)
+
+    if (!keyStore.containsAlias(keyAlias)) {
+        val keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+
+        val builder = KeyGenParameterSpec.Builder(
+            keyAlias,
+            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+        )
+            .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+            .setRandomizedEncryptionRequired(false)
+
+        keyGenerator.init(builder.build())
+        keyGenerator.generateKey()
+    }
+
+    return keyStore.getKey(keyAlias, null) as SecretKey
+}
