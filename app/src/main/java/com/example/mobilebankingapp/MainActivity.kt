f8a6d952a99cdd3e168733380a6060de38fbbee2
Ricu Alexandru <alexandruricu25@gmail.com>
2024-01-01 17:04:15 +0200

    Homescreen page WiP

--- app/src/main/java/com/example/mobilebankingapp/MainActivity.kt
+++ app/src/main/java/com/example/mobilebankingapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.mobilebankingapp
 
-import android.app.Activity.RESULT_OK
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -9,9 +8,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -22,14 +19,16 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.mobilebankingapp.firebase.FirebaseDbStore
+import com.example.mobilebankingapp.presentation.homepage.HomeViewModel
 import com.example.mobilebankingapp.presentation.login.GoogleAuthClient
 import com.example.mobilebankingapp.presentation.login.SignInViewModel
 import com.example.mobilebankingapp.screens.HomeScreen
 import com.example.mobilebankingapp.screens.SignInScreen
 import com.example.mobilebankingapp.ui.theme.MobileBankingAppTheme
 import com.google.android.gms.auth.api.identity.Identity
+import com.google.firebase.FirebaseApp
 import kotlinx.coroutines.launch
-import java.time.format.SignStyle
 
 class MainActivity : ComponentActivity() {
     private val googleAuthUiClient by lazy {
@@ -39,6 +38,7 @@ class MainActivity : ComponentActivity() {
         )
     }
     override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(this)
         super.onCreate(savedInstanceState)
         setContent {
             MobileBankingAppTheme {
@@ -47,9 +47,10 @@ class MainActivity : ComponentActivity() {
                     color = Color.Black
                 ) {
                     val navController = rememberNavController()
-                    NavHost(navController = navController , startDestination = "login") {
+                    NavHost(navController = navController , startDestination = "homepage") {
                         composable("login") {
                             val viewModel = viewModel<SignInViewModel>()
+
                             val state by viewModel.state.collectAsState()
                             val launcher = rememberLauncherForActivityResult(
                                 contract = ActivityResultContracts.StartIntentSenderForResult(),
@@ -91,19 +92,17 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         composable("homepage") {
-                            HomeScreen(navController = navController,
-                                userData = googleAuthUiClient.getSignedInUser(),
-                                onSignOut = {
-                                    lifecycleScope.launch {
-                                        googleAuthUiClient.signOut()
-                                        Toast.makeText(
-                                            applicationContext,
-                                            "Signed out successful",
-                                            Toast.LENGTH_LONG
-                                        ).show()
-                                        navController.navigate("login")
-                                    }
-                                }
+                            val viewModel = HomeViewModel(FirebaseDbStore(userId = googleAuthUiClient.getSignedInUser()?.userId))
+                            viewModel.googleAuthUiClient = googleAuthUiClient
+                            viewModel.userData = googleAuthUiClient.getSignedInUser()!!
+                            HomeScreen(
+                                userData = viewModel.userData,
+                                firebaseDataFlow = viewModel.userState,
+                                onTransferClick = {viewModel.onTransferClick(navController)},
+                                onAddOrChangeCard = {viewModel.onAddOrChangeCard(navController)},
+                                onBuyDifferentCurrency = {viewModel.onBuyDifferentCurrency(navController)},
+                                onHelpClick = {viewModel.onHelpClick(navController)},
+                                onLogout = {viewModel.onLogout(navController)}
                             )
                         }
                     }
